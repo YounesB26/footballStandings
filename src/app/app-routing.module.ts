c928bc88dd958965d4c551782ae1bd991ec14f51
Younes <johndoe@example.com>
2023-12-04 11:38:58 +0100

    code review

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { Standings } from './components/standings/app-standings.component';
 import { Fixtures } from './components/fixtures/app-fixtures.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: Standings },
+  { path: 'fixtures/:league/:team', component: Fixtures },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {
-  routes: Routes = [
-    { path: '', component: Standings },
-    { path: 'fixtures/:league/:team', component: Fixtures },
-  ];
-}
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Standings } from './components/standings/app-standings.component';
@@ -14,8 +13,7 @@ const router: AppRoutingModule = new AppRoutingModule();
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    RouterModule.forRoot(router.routes, { useHash: true }),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
